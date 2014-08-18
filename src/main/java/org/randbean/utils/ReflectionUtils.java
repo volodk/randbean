e47898a42e370a976033a63cc83106643c992914
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-08-18 16:43:40 +0300

    do not override not null values which where created during default instantiation (constructor based)

--- src/main/java/org/randbean/utils/ReflectionUtils.java
+++ src/main/java/org/randbean/utils/ReflectionUtils.java
@@ -3,6 +3,8 @@ package org.randbean.utils;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * 
@@ -25,18 +27,52 @@ public class ReflectionUtils {
         }
     }
 
-    public static Object newInstance(Class<?> clazz) {
-
+    public static void setIfNull(Object instance, Field field, Object value) {
         try {
-            for (Constructor<?> constr : clazz.getDeclaredConstructors()) {
-                if (constr.getGenericParameterTypes().length == 0) {
-                    return constr.newInstance(new Object[] {});
+            if (field.get(instance) == null) {
+                boolean wflag = field.isAccessible();
+                try {
+                    field.setAccessible(true);
+                    field.set(instance, value);
+                } finally {
+                    field.setAccessible(wflag);
                 }
             }
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            System.err.println("Cannot set value, details : " + e.getMessage());
+        }
+    }
+
+    public static Object newInstance(Class<?> clazz) {
+        try {
+            Constructor<?> c = null;
+            int n = clazz.getConstructors().length;
+            if (n > 1) {
+                Constructor<?> constuctors[] = Arrays.copyOf(clazz.getConstructors(), n);
+                Arrays.sort(constuctors, new Comparator<Constructor<?>>() {
+                    @Override
+                    public int compare(Constructor<?> c1, Constructor<?> c2) {
+                        return c2.getGenericParameterTypes().length - c1.getGenericParameterTypes().length;
+                    }
+                });
+                c = constuctors[0];
+            } else if (n == 1) {
+                c = clazz.getConstructors()[0];
+            } else {
+                c = clazz.getDeclaredConstructors()[0];
+            }
+
+            return createWithConstructor(c);
+
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private static Object createWithConstructor(Constructor<?> c) throws InstantiationException,
+            IllegalAccessException, InvocationTargetException {
+        return c.newInstance(new Object[]{});
     }
 
 }
